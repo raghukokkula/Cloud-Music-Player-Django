61c9f1f2060de1203dbc88eb40392d0e6592da8b
raghuvamsha <raghuvamshak@gmail.com>
2018-09-04 14:49:44 -0500

    New Generic View Method

--- music/urls.py
+++ music/urls.py
@@ -6,22 +6,9 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    path('', views.index, name='index'),
-
-    # /music/raghu/
-    path('raghu/', views.index1, name='index1'),
+    path('', views.IndexView.as_view(), name='index'),
 
     # /music/<album_id>/
-    path('<int:album_id>/', views.detail, name='detail'),
-
-    # /music/<album_id>/favorites
-    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
-
-    # /music/<album_id>/unfavorites
-    path('<int:album_id>/unfavorite/', views.unfavorite, name='unfavorite')
-
-
-
-
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 
 ]
