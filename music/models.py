3dd2c602592e372d4cf1e4386c82600710d8286f
raghuvamsha <raghuvamshak@gmail.com>
2018-09-07 18:10:36 -0500

    Hello this is my first commit from Pycharm

--- music/models.py
+++ music/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 
@@ -8,7 +10,10 @@ class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=1000)
+    album_logo = models.FileField()
+
+    def get_aboslute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
 
--- music/urls.py
+++ music/urls.py
@@ -8,7 +8,19 @@ urlpatterns = [
     # /music/
     path('', views.IndexView.as_view(), name='index'),
 
+    #register as a user
+    path('register/', views.UserFormView.as_view(), name='register'),
+
     # /music/<album_id>/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    #music/album/add/
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+
+    #music/album/2/
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update'),
+
+    #music/album/add/
+    path('album/<int:pk>/delete/', views.AlbumDelete.as_view(), name='album-delete')
 
 ]
